f487cd82a846da55efd48f01277f53eb2e49d6e6
Ruhid Ibadli <ruhid.ibadli@gmail.com>
2021-03-20 18:30:16 +0400

    'ch4'

--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #User management
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     #Local apps
-    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
 ]
